0c8add0f5f984b01f299f9a4fc8552e6038cafd8
AnakTentara <82577660+AnakTentara@users.noreply.github.com>
2025-01-20 22:13:52 +0700

    updated

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import authController from '../controllers/auth.controller';
-import authMiddleware from "../middlewares/auth.middleware.ts";
+import authMiddleware from "../middlewares/auth.middleware";
 
 const router = express.Router();
 
