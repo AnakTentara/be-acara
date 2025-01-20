009b49a041bbcef95e932d96efdae93bbafc76c5
AnakTentara <82577660+AnakTentara@users.noreply.github.com>
2025-01-20 22:07:43 +0700

    updated

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
 import express from 'express';
 import router from './routes/api';
+import bodyParser from 'body-parser';
+import db from "./utils/database";
 
-const app = express();
-const PORT = 3000;
+async function init() {
+    try {
+        const result = await db();
+        console.log("Database Status: ", result);
+        const app = express();
+        const PORT = 3000;
 
-app.use('/api', router);
+        app.use(bodyParser.json());
+        app.use('/api', router);
 
+        app.listen(PORT, () => {
+            console.log(`server runned on http://localhost:${PORT}`)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
 
-app.listen(PORT, () => {
-    console.log(`server runned on http://localhost:${PORT}`)
-})
+init();
