de1c5f9774706402c633d67fdcf5d509b545c8bf
AnakTentara <82577660+AnakTentara@users.noreply.github.com>
2025-01-20 22:27:18 +0700

    updated

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,12 @@ async function init() {
         const app = express();
         const PORT = 3000;
 
+        app.use('/', (req, res) => {
+            res.status(200).json({
+                message: "Welcome to the API!",
+                data: null,
+            })
+        });
         app.use(bodyParser.json());
         app.use('/api', router);
 
